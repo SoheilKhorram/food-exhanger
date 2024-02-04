8ac26916d3e238a439be978dd1688e0d586ee140
Soheil Khorram <soheil.khorram12@gmail.com>
2024-02-04 12:25:31 +0330

    using telegram emoji

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ function App() {
         </a>
         <a href='https://react.dev' target='_blank'>
           <img src={reactLogo} className='logo react' alt='React logo' />
+          🍟
         </a>
       </div>
       <h1>Vite + React</h1>
