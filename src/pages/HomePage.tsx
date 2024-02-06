8ab5940eb236e55a56d86734c5f266e36ec93774
Soheil Khorram <soheil.khorram12@gmail.com>
2024-02-06 14:55:46 +0330

    adding some animations

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,32 +1,52 @@
-import { useEffect, useRef } from "react"
-import styles from "./HomePage.module.css"
-import lottie from 'lottie-web';
-import GuyAnim from "../guy.json"
+import Lottie, { LottieRefCurrentProps } from 'lottie-react';
+import { useEffect, useRef } from 'react';
+import CircleAnim from '../assets/circle.json';
+import GuyAnim1 from '../assets/man2.json';
+import WomanAnim1 from '../assets/woman2.json';
+import styles from './HomePage.module.css';
 
 const HomePage = () => {
-    const container = useRef<HTMLDivElement | null>(null)
+  const container = useRef<LottieRefCurrentProps>(null);
 
-    useEffect(() => {
-        if (container.current) {
-            lottie.loadAnimation({
-                animationData: GuyAnim,
-                autoplay: true,
-                container: container.current,
-                loop: true,
-                renderer: "svg",
-            })
-        }
-    }, [])
-    
-    return (
-        <div>
-            <div className={styles["exchange_food_options__buttons"]}>
-                <button>درخواستی</button>
-                <button>واگذاری</button>
-                <button>تبادل</button>
-            </div>
-            <div ref={container} id="animation-container"></div>
-        </div>
-    )
-}
-export default HomePage
+  const handleClick = () => {
+    container.current?.goToAndPlay(4);
+  };
+
+  useEffect(() => {
+    container.current?.setSpeed(0.5);
+  }, []);
+
+  return (
+    <div>
+      <div className={styles['exchange_food_options__buttons']}>
+        <button onClick={handleClick}>درخواستی</button>
+        <button>واگذاری</button>
+        <button>تبادل</button>
+      </div>
+      <Lottie
+        lottieRef={container}
+        animationData={GuyAnim1}
+        autoPlay={true}
+        loop={true}
+        initialSegment={[35, 110]}
+      />
+      <div style={{ position: 'relative' }}>
+        <Lottie
+          lottieRef={container}
+          animationData={WomanAnim1}
+          autoPlay={true}
+          loop={true}
+          initialSegment={[35, 110]}
+        />
+        <Lottie
+          lottieRef={container}
+          animationData={CircleAnim}
+          autoPlay={true}
+          loop={true}
+          style={{ position: 'absolute', top: 0, color: 'red' }}
+        />
+      </div>
+    </div>
+  );
+};
+export default HomePage;
