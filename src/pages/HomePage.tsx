49192868068b5e6f89596c8e2d3eefff87400b2a
Soheil Khorram <soheil.khorram12@gmail.com>
2024-02-21 11:04:57 +0330

    testing the photo of the user

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,42 +1,44 @@
-import Lottie from 'lottie-react'
+import Lottie from 'lottie-react';
 
-import ManAnimation from '../assets/man.json'
-import WomanAnimation from '../assets/woman.json'
-import styles from './HomePage.module.css'
-import { useState } from 'react'
-import Exchange from '../components/Exchange'
-import Requested from '../components/Requested'
-import Transfer from '../components/Transfer'
-import WebApp from '@twa-dev/sdk'
+import WebApp from '@twa-dev/sdk';
+import { useState } from 'react';
+import ManAnimation from '../assets/man.json';
+import WomanAnimation from '../assets/woman.json';
+import Exchange from '../components/Exchange';
+import Requested from '../components/Requested';
+import Transfer from '../components/Transfer';
+import styles from './HomePage.module.css';
 // import Modal from '../components/Modal'
 // import WebApp from '@twa-dev/sdk'
 
-type FoodOptions = 'requested' | 'transfer' | 'exchange'
+type FoodOptions = 'requested' | 'transfer' | 'exchange';
 
 const HomePage = () => {
-  const [isMan, setIsMan] = useState<boolean | undefined>(undefined)
-  const [foodOption, setFoodOption] = useState<FoodOptions>('exchange')
+  const [isMan, setIsMan] = useState<boolean | undefined>(undefined);
+  const [foodOption, setFoodOption] = useState<FoodOptions>('exchange');
 
   const handleFoodOptions = (option: FoodOptions) => {
     switch (option) {
       case 'requested':
-        setFoodOption('requested')
-        break
+        setFoodOption('requested');
+        break;
       case 'transfer':
-        setFoodOption('transfer')
-        break
+        setFoodOption('transfer');
+        break;
       case 'exchange':
-        setFoodOption('exchange')
-        break
+        setFoodOption('exchange');
+        break;
     }
-  }
+  };
+
+  console.log(WebApp.initDataUnsafe.user);
 
   return (
     <>
       <div className={styles['home-page']}>
-        <section className={styles["man_or_woman"]}>
+        <section className={styles['man_or_woman']}>
           <div
-            className={styles["man_or_woman--man"]}
+            className={styles['man_or_woman--man']}
             onClick={() => setIsMan(true)}
           >
             <Lottie
@@ -44,12 +46,16 @@ const HomePage = () => {
               autoPlay={true}
               loop={true}
               initialSegment={[35, 110]}
-              style={{ zIndex: 10, position: "relative" }}
+              style={{ zIndex: 10, position: 'relative' }}
             />
-            <div className={`${styles["man_or_woman--woman-border"]} ${isMan === false ? styles["woman-border-color"] : null} `} />
+            <div
+              className={`${styles['man_or_woman--woman-border']} ${
+                isMan === false ? styles['woman-border-color'] : null
+              } `}
+            />
           </div>
           <div
-            className={styles["man_or_woman--woman"]}
+            className={styles['man_or_woman--woman']}
             onClick={() => setIsMan(false)}
           >
             <Lottie
@@ -58,13 +64,38 @@ const HomePage = () => {
               loop={true}
               initialSegment={[35, 110]}
             />
-            <div className={`${styles["man_or_woman--man-border"]} ${isMan === true ? styles["man-border-color"] : null} `} />
+            <div
+              className={`${styles['man_or_woman--man-border']} ${
+                isMan === true ? styles['man-border-color'] : null
+              } `}
+            />
           </div>
         </section>
         <div className={styles['code_options']}>
-          <button onClick={() => handleFoodOptions('exchange')} className={`button unselected ${foodOption === "exchange" ? "selected" : null}`}>تبادل</button>
-          <button onClick={() => handleFoodOptions('requested')} className={`button unselected ${foodOption === "requested" ? "selected" : null}`}>درخواستی</button>
-          <button onClick={() => handleFoodOptions('transfer')} className={`button unselected ${foodOption === "transfer" ? "selected" : null}`}>واگذاری</button>
+          <button
+            onClick={() => handleFoodOptions('exchange')}
+            className={`button unselected ${
+              foodOption === 'exchange' ? 'selected' : null
+            }`}
+          >
+            تبادل
+          </button>
+          <button
+            onClick={() => handleFoodOptions('requested')}
+            className={`button unselected ${
+              foodOption === 'requested' ? 'selected' : null
+            }`}
+          >
+            درخواستی
+          </button>
+          <button
+            onClick={() => handleFoodOptions('transfer')}
+            className={`button unselected ${
+              foodOption === 'transfer' ? 'selected' : null
+            }`}
+          >
+            واگذاری
+          </button>
         </div>
         {foodOption === 'exchange' && <Exchange />}
         {foodOption === 'requested' && <Requested />}
@@ -73,7 +104,7 @@ const HomePage = () => {
           somthing
         </Modal> */}
         <div>Eruda is here</div>
-        <img src={WebApp.initDataUnsafe.user?.photo_url} alt="usererer" />
+        <img src={WebApp.initDataUnsafe.user?.photo_url} alt='usererer' />
         <div>{WebApp.initDataUnsafe.user?.photo_url}</div>
         <div>{WebApp.initDataUnsafe.receiver?.photo_url}</div>
         <div>{WebApp.initDataUnsafe.chat?.photo_url}</div>
@@ -81,6 +112,6 @@ const HomePage = () => {
         {/* <div>{WebApp.initData.photo_url || "URL"}</div> */}
       </div>
     </>
-  )
-}
-export default HomePage
+  );
+};
+export default HomePage;
